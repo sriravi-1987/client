5223ec36eb25d0390265754b453621bd3d0f7300
sriravi-1987 <sriram.ravi1002@gmail.com>
2021-07-13 18:01:24 +0530

    my changes

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -13,11 +13,11 @@ export class NavComponent implements OnInit {
   constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
+    this.getCurrentUser();
   }
 
   login(){
     this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
       this.loggedIn=true;
     }, error => {
       console.log(error);
@@ -26,6 +26,16 @@ export class NavComponent implements OnInit {
 
   logout()
   {
+    this.accountService.logout();
     this.loggedIn=false;
+  }
+
+  getCurrentUser()
+  {
+    this.accountService.currentUser$.subscribe(user => {
+      this.loggedIn=!!user;
+    },error => {
+      console.log(error);
+    })
   }
 }
