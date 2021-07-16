d450ac83fb2cb97f535ac20c92fb51566b071def
sriravi-1987 <sriram.ravi1002@gmail.com>
2021-07-17 00:20:36 +0530

    latest ui changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
 
@@ -9,18 +9,17 @@ import { AccountService } from './_services/account.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'The Dating App';
-  users:any;
+  title = 'The Dating app';
+  users: any;
 
-  constructor(private accountService: AccountService){}
-  
+  constructor(private accountService: AccountService) {}
+
   ngOnInit() {
     this.setCurrentUser();
   }
 
-  setCurrentUser()
-  {
-    const user: User=JSON.parse(localStorage.getItem('user') || '{}');
+  setCurrentUser() {
+   const user: User = JSON.parse(localStorage.getItem('user') || '{}');
     this.accountService.setCurrentUser(user);
-  }  
+  }
 }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -7,35 +11,25 @@ import { AccountService } from '../_services/account.service';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-  model:any = {}
-  loggedIn: boolean=false;
+  model: any = {}
 
-  constructor(private accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, 
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
-    this.getCurrentUser();
   }
 
-  login(){
+  login() {
     this.accountService.login(this.model).subscribe(response => {
-      this.loggedIn=true;
+      this.router.navigateByUrl('/members');
     }, error => {
       console.log(error);
+      this.toastr.error(error.error);
     })
   }
 
-  logout()
-  {
+  logout() {
     this.accountService.logout();
-    this.loggedIn=false;
-  }
-
-  getCurrentUser()
-  {
-    this.accountService.currentUser$.subscribe(user => {
-      this.loggedIn=!!user;
-    },error => {
-      console.log(error);
-    })
+    this.router.navigateByUrl('/')
   }
 }
